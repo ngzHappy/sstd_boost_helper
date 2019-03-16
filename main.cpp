6c9dfb6ea0727a8e2180c1065416f21d19413a62
ngzHappy <zhaixueqiang@hotmail.com>
2019-03-16 17:45:33 +0800

    +construct

---
+++ main.cpp
@@ -0,0 +1,299 @@
+﻿
+#include <string>
+#include <memory>
+#include <string_view>
+#include <type_traits>
+
+using namespace std::string_literals;
+using namespace std::string_view_literals;
+
+#include <utility>
+#include <algorithm>
+
+#include <fstream>
+#include <iostream>
+
+#if __has_include(<filesystem>)
+#include <filesystem>
+namespace fs = std::filesystem;
+#else
+#include <experimental/filesystem>
+namespace fs = std::experimental::filesystem;
+#endif
+
+#include <list>
+#include <vector>
+
+#include <regex>
+
+#if __has_include(<execution>)
+#include <execution>
+#endif
+
+#include <cctype>
+
+#include <map>
+#include <functional>
+
+namespace thisMainFile {
+
+    class ifstream : public std::ifstream {
+        using super = std::ifstream;
+    public:
+        inline ifstream(const fs::path & arg);
+    };
+
+    class ofstream : public std::ofstream {
+        using super = std::ofstream;
+    public:
+        inline ofstream(const fs::path & arg);
+    };
+
+    class CopyItem {
+    public:
+        fs::path from;
+        fs::path to;
+        inline CopyItem() = default;
+        inline CopyItem(const CopyItem &) = default;
+        inline CopyItem(CopyItem &&) = default;
+        inline CopyItem &operator=(CopyItem &&) = default;
+        inline CopyItem &operator=(const CopyItem &) = default;
+        template<typename A, typename B>
+        inline CopyItem(A&&, B&&);
+    };
+
+    class CopyInfo {
+    public:
+        std::list< CopyItem > dirs;
+        std::list< CopyItem > files;
+    };
+
+    inline CopyInfo createCopyInfo(fs::path from, fs::path to);
+    inline void createDirs(const CopyInfo &);
+    inline void copyFiles(const CopyInfo &);
+    inline void rawCopyAFile(const CopyItem &);
+
+}/*namespace thisMainFile*/
+
+
+int main(int argc, char ** argv) {
+
+    if (argc != 3) {
+        return -1;
+    }
+
+    auto varCopyInfo = thisMainFile::createCopyInfo(argv[1], argv[2]);
+    thisMainFile::createDirs(varCopyInfo);
+    thisMainFile::copyFiles(varCopyInfo);
+
+    return 0;
+
+}
+
+namespace thisMainFile {
+
+    inline std::string_view getEndl() {
+        return "\n"sv;
+    }
+
+    inline ifstream::ifstream(const fs::path & arg) :
+        super(arg.wstring(), std::ios::binary) {
+        this->sync_with_stdio(false);
+    }
+
+    inline ofstream::ofstream(const fs::path & arg) :
+        super(arg.wstring(), std::ios::binary) {
+        this->sync_with_stdio(false);
+    }
+
+    inline CopyInfo createCopyInfo(fs::path argFrom, fs::path argTo) {
+
+        std::list< CopyItem > varDirs;
+        varDirs.emplace_back(std::move(argFrom), argTo / u8R"(sstd)"s);
+
+        CopyInfo varAns;
+
+        while (!varDirs.empty()) {
+
+            auto & varItem = varAns.dirs;
+            varItem.splice(varItem.end(),
+                varDirs,
+                varDirs.begin());
+
+            auto & var = varItem.back();
+
+            fs::directory_iterator varPos{ var.from };
+            fs::directory_iterator varEnd;
+
+            for (; varPos != varEnd; ++varPos) {
+
+                const auto & varPath = varPos->path();
+                const auto varFileName = varPath.filename();
+
+                if (fs::is_directory(varPos->status())) {
+                    varDirs.emplace_front(varPath,
+                        var.to / varFileName);
+                    continue;
+                } else {
+                    varAns.files.emplace_back(varPath,
+                        var.to / varFileName);
+                    continue;
+                }
+
+            }
+
+        }
+
+        return std::move(varAns);
+
+    }
+
+    inline void createDirs(const CopyInfo & arg) {
+        for (const auto & varI : arg.dirs) {
+            try {
+                fs::create_directories(varI.to);
+            } catch (...) {
+                continue;
+            }
+        }
+    }
+
+    inline void copyAFile(const CopyItem & varI) {
+
+        if (!varI.from.has_extension()) {
+            rawCopyAFile(varI);
+            return;
+        }
+
+        {
+            const auto varExtension
+                = varI.from.extension().wstring();
+            const static
+                std::wregex varCheckIsNotBinary{
+                    LR"121([.](?:hpp)|(?:h)|(?:cxx)|(?:cpp)|(?:c)|(?:hxx)|(?:txt))121"s ,
+                    std::regex::ECMAScript | std::regex::icase
+            };
+            if (!std::regex_match(varExtension, varCheckIsNotBinary)) {
+                rawCopyAFile(varI);
+                return;
+            }
+        }
+
+        ofstream varOut{ varI.to };
+        ifstream varIn{ varI.from };
+
+        /*增加UTF8-bom*/
+        varOut << "\xEF\xBB\xBF"sv;
+
+        /*用于保存过去已经读取的line*/
+        std::list< std::string > varFileLines;
+
+        while (varIn.good()) {
+
+            std::string varLine;
+            std::string varNewLine;
+            std::getline(varIn, varLine);
+
+            while (!varLine.empty()) {/*删除行尾空行*/
+                auto varChar = varLine.back();
+                if ((varChar > 0) && (varChar < char(127)) && std::isspace(varChar)) {
+                    varLine.pop_back();
+                } else {
+                    break;
+                }
+            }
+
+            /*结束时保存当前行*/
+            class LineLock {
+                std::string line;
+                std::list< std::string > * const lines;
+            public:
+                inline LineLock(std::string & arg, std::list<std::string> * v) :
+                    line(arg), lines(v) {
+                }
+                inline ~LineLock() {
+                    lines->push_back(std::move(line));
+                }
+            }varLineLock{ varLine , &varFileLines };
+
+            if (varLine.empty()) {
+                varOut << getEndl();
+                continue;
+            }
+
+            if ((!varFileLines.empty()) && varFileLines.back().empty()) {/*尝试替换孤行#define*/
+                using convert_string_map_t =
+                    std::map< std::string, std::function<std::string(std::string_view)> >;
+                const static convert_string_map_t varConvertStringMap = []()->convert_string_map_t {
+                    convert_string_map_t varAns;
+                    varAns.emplace("#define "s, [](std::string_view)->std::string {
+                        return u8R"(#ifndef /*added ...*/
+#define /*added ...*/
+#endif  /*added ...*/)"s;  });
+                    return std::move(varAns);
+                }();
+                auto varPos = varConvertStringMap.find(varLine);
+                if (varPos != varConvertStringMap.end()) {
+                    varOut << varPos->second(varLine) << getEndl();
+                    continue;
+                }
+            }
+
+            if (varLine.find("boost"sv) != std::string::npos) {
+
+                const static std::regex  varReplaceRegex{
+                    u8R"rrr((["<(])boost([/\\]))rrr"s ,
+                    std::regex::ECMAScript };
+                const static auto varTarget = u8R"vvv($1sstd$2boost$2)vvv"s;
+
+                varNewLine = std::regex_replace(varLine,
+                    varReplaceRegex, varTarget,
+                    std::regex_constants::format_first_only);
+
+#ifdef _DEBUG
+                if ((std::string::npos != varNewLine.find(u8R"(boost\)"sv)) && (varLine != varNewLine)) {
+                    std::cout << varNewLine << std::endl;
+                }
+#endif
+
+            } else {
+                varNewLine = std::move(varLine);
+            }
+
+            varOut
+                << varNewLine
+                << getEndl();
+
+        }
+
+    }
+
+    inline void copyFiles(const CopyInfo & arg) {
+
+#if __has_include(<execution>)
+        std::for_each(std::execution::par,
+            arg.files.begin(),
+            arg.files.end(),
+            &copyAFile);
+#else
+        std::for_each(arg.files.begin(), arg.files.end(), &copyAFile);
+#endif
+    }
+
+    inline void rawCopyAFile(const CopyItem & arg) {
+        ofstream varOut{ arg.to };
+        ifstream varIn{ arg.from };
+        varOut << varIn.rdbuf();
+    }
+
+    template<typename A, typename B>
+    inline CopyItem::CopyItem(A&& a, B&& b) :
+        from(std::forward<A>(a)),
+        to(std::forward<B>(b)) {
+    }
+
+}/*namespace thisMainFile*/
+
+
+
+
---
+++ sstd_boost_helper.pro
@@ -0,0 +1,25 @@
+
+TEMPLATE = app
+
+QT -= gui
+QT -= core
+
+CONFIG += c++17
+CONFIG += console
+
+SOURCES += main.cpp
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
